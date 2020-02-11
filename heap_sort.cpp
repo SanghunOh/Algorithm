8ede14951169d3ae635765af80945239c665bda9
poiki1219 <ohsang1219@naver.com>
2020-02-11 14:14:46 +0000

    2/11

--- heap_sort.cpp
+++ heap_sort.cpp
@@ -1,11 +1,21 @@
 #include	<iostream>
 
-int N;
-
-void heapify(int arr[], int h){
+void heapify(int arr[], int h, int N){
 	int k;
 	
-	k = arr[2*h] > arr[2*h+1] ? 2*h : 2*h+1;
+	k = h*2;
+	if(k > N)
+		return;
+	if(k == N){	
+		int t = arr[k];
+		arr[k] = arr[h];
+		arr[h] = t;
+	
+		heapify(arr, k, N);
+		return;
+	}
+
+	k = arr[k] > arr[k+1] ? k : k+1;
 
 	if(arr[h] >= arr[k])
 		return;
@@ -14,27 +24,31 @@ void heapify(int arr[], int h){
 	arr[k] = arr[h];
 	arr[h] = t;
 
-	heapify(arr, k);
+	heapify(arr, k, N);
 }
 
-void heap_sort(int arr[]){
-	for(int i=N ; i>=2 ; i++){
+void build_heap(int arr[], int N){
+	for(int i=N/2 ; i>=1 ; i--)
+		heapify(arr, i, N);
+}
+
+void heap_sort(int arr[], int& N){
+	build_heap(arr, N);
+	for(int i=N ; i>=2 ; i--){
 		int t = arr[i];
 		arr[i] = arr[1];
 		arr[1] = t;
 
 		N--;
-		heapify(arr, 1);
+		heapify(arr, 1, N);
 	}
 }
 
-void build_heap(int arr[]){
-	for(int i=N/2 ; i>=1 ; i++)
-		heapify(arr, i);
-}
 
 int main(){
 	int *arr;
+	int N;
+	int N_;
 
 	std::cin >> N;
 	arr = new int[N+1];
@@ -42,9 +56,10 @@ int main(){
 	for(int i=1 ; i<=N ; i++)
 		std::cin >> arr[i];
 
-	heap_sort(arr);
+	N_ = N;
+	heap_sort(arr, N);
 
-	for(int i=1 ; i<=N ; i++)
+	for(int i=1 ; i<=N_ ; i++)
 		std::cout << arr[i] << " ";
 	std::cout << std::endl;
 
