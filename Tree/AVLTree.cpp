138fdec7a026f0cff00c16f368f83643a253cd15
poiki1219 <ohsang1219@naver.com>
2020-03-07 05:50:57 +0000

    3/7

--- Tree/AVLTree.cpp
+++ Tree/AVLTree.cpp
@@ -6,6 +6,7 @@ class Node{
 protected:
 	Node* left;
 	Node* right;
+	Node* parent;
 	int key;
 
 public:
@@ -29,12 +30,18 @@ public:
 		return right;
 	}
 
-	void makeRight(Node* par, Node* n){
-		par->right = n;
+	Node* getParent(){
+		return parent;
 	}
 
-	void makeLeft(Node* par, Node* n){
-		par->left = n;
+	void makeRight(Node* n){
+		n->parent = this;
+		right = n;
+	}
+
+	void makeLeft(Node* n){
+		n->parent = this;
+		left = n;
 	}
 
 	~Node(){
@@ -82,7 +89,7 @@ public:
 					
 				}
 				else{
-					temp->makeRight(temp, n);
+					temp->makeRight(n);
 				}
 			}
 			else{
@@ -90,14 +97,41 @@ public:
 				
 				}
 				else{
-					temp->makeLeft(temp, n);
+					temp->makeLeft(n);
 				}
 			}
 		}
 	}
 
-	void del(){
-		
+	void del(int data){
+		Node* del_node = search(data);
+
+		if(!del_node->getLeft() && !del_node->getRight()){
+			if(data > del_node->getParent()->getKey())
+				del_node->getParent()->makeRight(nullptr);
+			else
+				del_node->getParent()->makeLeft(nullptr);
+			delete del_node;
+		}
+		else if(!del_node->getLeft() || !del_node->getRight()){
+			Node* n = del_node->getLeft() ? del_node->getLeft() : del_node->getRight();
+
+			if(data > del_node->getParent()->getKey())
+				del_node->getParent()->makeRight(n);
+			else
+				del_node->getParent()->makeLeft(n);
+			delete del_node;
+		}
+		else{
+			Node* par = del_node->getParent();
+			Node* min = findMinimum(del_node);
+
+			min->getParent()->makeLeft(nullptr);
+
+			del_node->setKey(min->getKey());
+			
+			del(min->getKey());
+		}
 	}
 
 	Node* search(int data){
@@ -111,6 +145,30 @@ public:
 			else
 				temp = temp->getLeft();
 		}
+	}
+
+	Node* findMinimum(Node* n) {
+		while(n->getLeft())
+			 n = n->getLeft();
+		return n;
+	}
+
+	Node* findMaximum(Node* n){
+		while(n->getRight())
+			n = n->getRight();
+		return n;
+	}
+
+	Node* findSuccessor(Node* N){
+		if(N->getRight())
+			return findMinimum(N->getRight());
+	
+		Node* y = N->getParent();
+	while(y && N == y->getRight()){
+			N = y;
+			y = y->getParent();
+		}
+		return y;
 	}
 };
 
