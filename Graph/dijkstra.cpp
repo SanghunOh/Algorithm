3d4b9329b1fdacf3dca1d8df516cbe80998fd1a2
poiki1219 <ohsang1219@naver.com>
2020-03-22 11:20:45 +0000

    3/22

--- Graph/dijkstra.cpp
+++ Graph/dijkstra.cpp
@@ -15,35 +15,32 @@ struct comp{
 };
 
 void Dijkstra(std::vector<edge>*& graph, const int N, const int M, const int s){
-	edge t{s, s, 0};
 	std::vector<int> d(N, INF);
 	std::vector<int> pre(N, -1);
 	std::vector<bool> checked(N, false);
-	std::priority_queue<edge, std::vector<edge>, comp> pq;
+	std::priority_queue<std::pair<int, int>> pq;
+	pq.push(std::make_pair(s, 0));
 
 	d[s] = 0;
-	checked[s] = true;
-	pq.push(t);
-	printf("1\n");
 
 	while(!pq.empty()){
-		t = pq.top();
-		int u = t.v;
-		int dist = -t.w;
+		int u = pq.top().first;
+		int distance = -pq.top().second;
+		pq.pop();
 
-		checked[u] = true;
-		if(d[u] < dist)
+		if(d[u] < distance)
 			continue;
 		for(int i=0 ; i<graph[u].size() ; i++){
 			int v = graph[u][i].v;
 			
-			if(dist + graph[u][i].w > d[v] && !checked[v]){
-				d[v] = dist + graph[u][i].w;
+			if(d[v] > distance + graph[u][i].w){
+				d[v] = distance + graph[u][i].w;
 				pre[v] = u;
-				pq.push(edge{0, v, -d[v]});
+				pq.push(std::make_pair(v, -d[v]));
 			}
 		}
 	}
+
 
 	for(int i=0 ; i<N ; i++){
 		std::cout << "TO " << i << " : " << d[i] << std::endl;
